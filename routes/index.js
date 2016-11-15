4df988c40e8e42440cae5f2efe90f61ea7e3f412
Eric Fawcett <eric.fawcett@nhrec.org>
2016-11-15 10:14:00 -0500

    enabled passport authentication (basic)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@
 * quarry routes
 *
 **/
+var passport = require('passport');
+
 module.exports = function( app ) {
+  /* Auth on all routes */
+  app.all('*', passport.authenticate('basic'), function( req, res, next ) {
+    next();
+  });
+
   /*==========  Landing  ==========*/
   app.get('/', landing );
 
